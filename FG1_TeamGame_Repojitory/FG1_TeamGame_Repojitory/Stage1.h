85e072efc1134145998c762a33a95e8ece0fbb60
KuonAbe <60372806+KuonAbe@users.noreply.github.com>
2020-11-09 17:01:22 +0900

    Stage1クラスの大まかな中身追加

---
+++ FG1_TeamGame_Repojitory/FG1_TeamGame_Repojitory/Stage1.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<DxLib.h>
+#include"Base_Scene.h"
+#include"Scene_Manager.h"
+//#include"Stage1_ground.h"
+//#include"Player.h"
+//#include"Enemy.h"
+//#include"Stage1_DayTime.h"
+
+class Stage1 :public Base_Scene
+{
+private:
+	//プレイヤー用変数
+	int player_Pos_X;
+	int player_Pos_Y;
+
+	//エネミー用変数
+	int enemy_Pos_X;//エネミーのポジション入力用
+	int enemy_Pos_Y;//エネミーのポジション入力用
+	int enemy_array[2];
+
+	//昼用
+
+	//夜用
+
+
+public:
+	Stage1();
+	~Stage1();
+
+	void Init()override;
+	void Update()override;
+	void Draw()override;
+	void Finish()override;
+
+	////マップチップ
+	//Stage1_ground stage1_Ground;
+	//Stage1_DayTime stage1_DayTime;
+	////プレイヤー
+	//Player player;
+	//Enemy enemy_01;
+	//Enemy enemy_02;
+
+	Scene_Manager* scene_Manager;
+};
+
