14388db07b4e11029b87aaebc9b589da7e700c44
KuonAbe <60372806+KuonAbe@users.noreply.github.com>
2020-11-20 11:14:46 +0900

    多段ヒット防いだ

--- FG1_TeamGame_Repojitory/FG1_TeamGame_Repojitory/Enemy.cpp
+++ FG1_TeamGame_Repojitory/FG1_TeamGame_Repojitory/Enemy.cpp
@@ -10,6 +10,7 @@ void Enemy::Init(int enemy_Pos_X, int enemy_Pos_Y)
 	this->enemy_pos_Y = enemy_Pos_Y;
 	isEnemy_Dead = false;
 	Direction = false;
+	enemy_Attack_Count = 0;
 	enemy_Draw_Direction = RIGHT;
 	enemy_Right_Tex = LoadGraph("Villager.png");
 	enemy_Left_Tex = LoadGraph("Villager.png");
@@ -48,24 +49,28 @@ void Enemy::Update(int player_Pos_X, int player_Pos_Y)
 
 bool Enemy::Enemy_isAttack()
 {
-
 	if (isEnemy_Dead == false)
 	{
+		enemy_Attack_Count++;
 		//プレイヤーの位置確認用
 		//エネミーとプレイヤーの当たり判定
 		if (collision.Enemy_Collision(
 			enemy_pos_X, enemy_pos_Y,
 			enemyClass_player_Pos_X,
-			enemyClass_player_Pos_Y) == true)
+			enemyClass_player_Pos_Y) == true &&
+			enemy_Attack_Count >= 100)
 		{
 			DrawFormatString(1000, 400, (255, 255, 255), "player_Pos_X:%d", enemyClass_player_Pos_X);
 			DrawFormatString(1000, 415, (255, 255, 255), "player_Pos_Y:%d", enemyClass_player_Pos_Y);
+
+			if(enemy_Attack_Count>=101)
+			enemy_Attack_Count = 0;
+
 			//エネミーがプレイヤーに当たったら
 			//enemy_Draw_Direction = ATTACK;
 			return true;
 		}
 	}
-
 }
 
 bool Enemy::Enemy_isDead()
